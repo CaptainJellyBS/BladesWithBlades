0102af16824b05c1f434ae33ffacb66e867b395c
Mana <manakouwenberg@gmail.com>
2020-10-05 10:08:41 +0200

    Woop

---
+++ Blades With Blades/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float rotationSpeed, movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    void HandleInput()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime, Space.World);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime, Space.World);
+        }
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        }
+    }
+}
