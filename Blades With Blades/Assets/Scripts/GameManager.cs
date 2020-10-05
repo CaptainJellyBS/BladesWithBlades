39a6ba0373d4ce736d5e19eef978515c67e88340
Mana <manakouwenberg@gmail.com>
2020-10-05 14:40:03 +0200

    You now mow grass yay

---
+++ Blades With Blades/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject normalGrass;
+    public float minX, maxX, minZ, maxZ, offsetX, offsetZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnGrass();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnGrass()
+    {
+        for (float i = minX; i < maxX; i += offsetX)
+        {
+            for(float j = minZ; j < maxZ; j+=offsetZ)
+            {
+                Instantiate(normalGrass, new Vector3(i, 0, j), Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Blades With Blades/Assets/Scripts/NormalGrass.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalGrass : MonoBehaviour
+{
+    public float growSpeed;
+    public float minScale, maxScale;
+    public float secondsUntilGrow;
+    IEnumerator grow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grow = Grow();
+        StartCoroutine(grow);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("MowerBlade") || other.gameObject.CompareTag("Sword"))
+        {
+            StopCoroutine(grow);
+
+            transform.localScale = new Vector3(transform.localScale.x, minScale, transform.localScale.z);
+            grow = Grow();
+            StartCoroutine(grow);
+        }
+    }
+
+    IEnumerator Grow()
+    {
+        yield return new WaitForSeconds(secondsUntilGrow);
+        float siz = transform.localScale.y;
+        while (siz < maxScale)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + growSpeed, transform.localScale.z);
+            siz = transform.localScale.y;
+            yield return null;
+        }
+
+    }
+}
